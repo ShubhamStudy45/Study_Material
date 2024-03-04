fc4973ff43c1f3993c2062bcacfb700fa67b5c4d
ShubhamStudy45 <sableshubham890@gmail.com>
2024-03-04 17:06:27 +0530

    Object in JS and its type

---
+++ DAY03/app1/page2.js
@@ -0,0 +1,23 @@
+//Object 
+
+//Object creation in JS
+// there are 3 ways to create Object in JS.
+
+/**
+ * - Object literal (JSON)
+ * - Using Object root function
+ * - Using Contructor function
+ */
+
+// * - Object literal (JSON)
+
+function function1(){
+    
+}
+
+
+
+
+
+
+
