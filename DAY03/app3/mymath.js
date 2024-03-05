e01c48eb45ad847b18930dd333ca56a4ae73f173
ShubhamStudy45 <sableshubham890@gmail.com>
2024-03-05 15:16:12 +0530

    module and export concept

---
+++ DAY03/app3/mymath.js
@@ -0,0 +1,23 @@
+function addition( a, b ){
+
+    console.log(`Addition : ${ a + b }`)
+}
+
+function substraction( a, b ){
+
+    console.log(`substraction : ${ a - b }`)
+}
+function multiply( a, b ){
+
+    console.log(`multiply : ${a * b}`)
+}
+function division( a, b ){
+
+    console.log(`division : ${a / b}`)
+}
+
+
+addition(10,30)
+substraction(40,10)
+multiply(20,30)
+division(40,5)
