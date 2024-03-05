a800119aacc9c6e9b553106c37462f381b852b7e
ShubhamStudy45 <sableshubham890@gmail.com>
2024-03-05 15:27:53 +0530

    create exports object for add,sub,mul,div

--- DAY03/app3/mymath.js
+++ DAY03/app3/mymath.js
@@ -14,10 +14,24 @@ function multiply( a, b ){
 function division( a, b ){
 
     console.log(`division : ${a / b}`)
+    
 }
 
+//We want to export this function in another module(page1.js). for that we need to use module and export concept
 
-addition(10,30)
-substraction(40,10)
-multiply(20,30)
-division(40,5)
+// addition(10,30)
+// substraction(40,10)
+// multiply(20,30)
+// division(40,5)
+
+// console.log(module)
+
+//We have to export the function that we need to use in another module or file (page1.js)
+
+module.exports = {
+    add : addition,
+    sub : substraction,
+    multi : multiply,
+    div : division,
+}
+
