5ac824e6b8476a05340f5d825dd0ac867971938e
ShubhamStudy45 <sableshubham890@gmail.com>
2024-03-05 15:25:17 +0530

    Call mymath.js in Page1.js

---
+++ DAY03/app3/page1.js
@@ -0,0 +1,10 @@
+//load the mymath.js file here and call the function by using require
+
+const math = require('./mymath')
+
+// console.log(math)
+
+math.add(20,34)
+math.sub(34,6)
+math.multi(55,75)
+math.div(20,5)
