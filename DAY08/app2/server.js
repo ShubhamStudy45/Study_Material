21919f697178774660351d77a5f784302934700a
Mini_Projects <sableshubham890@gmail.com>
2024-03-21 01:43:41 +0530

    Server file

--- DAY08/app2/server.js
+++ DAY08/app2/server.js
@@ -1,16 +1,56 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const config = require('./config')
 const cors = require('cors')
 const userRouter = require('./router/user')
+const categoryRouter = require('./router/category')
+const companyRouter = require('./router/category')
+const productRouter = require('./router/product')
+const cartRouter = require('./router/cart')
+const orderRouter = require('./router/order')
 const app = express()
 
 app.use(bodyParser.json())
 app.use(cors('*'))
-app.use(bodyParser.urlencoded())
-app.use('/user',userRouter)
+// app.use(bodyParser.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 
+app.use((request, response,next)=>{
+
+    if(request.url.startsWith('/user/signin') ||
+    request.url.startsWith('/user/signup')||
+    request.url.startsWith('/user/admin/')){
+
+        next()
+    }else{
+
+        const token = request.headers['token']
+        // console.log(token)
+        try {
+            
+            const data = jwt.verify(token,config.secrete)
+            const payload = data['id']
+            // console.log(data)
+            request.userId = payload
+            next()
+        } catch (ex) {
+            response.send({
+                status : 'error',
+                error : "Unathorized access..!!"
+            })
+        }
+
+    }
+})
+app.use('/user',userRouter)
+app.use('/category',categoryRouter)
+app.use('/company',companyRouter)
+app.use('/product',productRouter)
+app.use('/cart',cartRouter)
+app.use('/order',orderRouter)
 app.listen(3000,'0.0.0.0',()=>{
     console.log('Server started on port 3000')
 })
 
-//47:30
+//1:26:34
