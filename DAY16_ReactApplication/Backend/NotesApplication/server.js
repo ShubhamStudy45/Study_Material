2d5139d07d1dd18e02efb414965a9b37c79023b9
Mini_Projects <sableshubham890@gmail.com>
2024-04-10 03:25:31 +0530

    cors added

--- DAY16_ReactApplication/Backend/NotesApplication/server.js
+++ DAY16_ReactApplication/Backend/NotesApplication/server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 const app = express()
-
+app.use(cors("*"))
 app.use(bodyParser.json())
 //Application Routes
 const userRoutes = require("./routes/user")
